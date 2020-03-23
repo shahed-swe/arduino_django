5fe1aa89202bd8dd419999eb537a25858a768d64
Shahed Talukder <shahedtalukder51@gmail.com>
2020-03-23 15:04:56 +0600

    hello

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    email = models.EmailField()
